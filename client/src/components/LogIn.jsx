ca320ed7155b76bd4f35e58e2737b373cd9b170d
betty2310 <huynh.dh2310@gmail.com>
2023-06-03 11:21:21 +0700

    fix: use tailwind component

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -2,6 +2,13 @@ import axios from "axios";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Navigate, useNavigate } from "react-router-dom";
+import {
+  Card,
+  Input,
+  Checkbox,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
 
 function LogIn() {
   const {
@@ -23,29 +30,45 @@ function LogIn() {
     // axios.post("http://localhost:8000/login", values).then((res) => {
     //   Navigate("/");
     // });
-    // console.log(values);
+    console.log(values);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <p>Tài khoản</p>
-      <input
-        name="account"
-        {...register("account", { required: true })}
-        onChange={(e) => setValues({ ...values, account: e.target.value })}
-      />
-      {errors.account && <p>Bạn phải nhập tài khoản</p>}
-      <p>Mật khẩu</p>
-      <input
-        name="password"
-        {...register("password", { required: true })}
-        onChange={(e) => setValues({ ...values, password: e.target.value })}
-      />
-
-      {errors.lastName && <p>Bạn phải nhập mật khẩu</p>}
-
-      <input type="submit" />
-    </form>
+    <Card
+      color="transparent"
+      className="items-center flex h-screen top-10"
+      shadow={false}
+    >
+      <Typography variant="h4" color="blue-gray">
+        Đăng nhập vào hệ thống
+      </Typography>
+      <Typography variant="h1" color="blue" textGradient>
+        Quản lý tổ dân phố
+      </Typography>
+      <form
+        className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
+        onSubmit={handleSubmit}
+      >
+        <div className="mb-4 flex flex-col gap-6">
+          <Input
+            size="lg"
+            label="Tài khoản"
+            name="account"
+            onChange={(e) => setValues({ ...values, account: e.target.value })}
+          />
+          <Input
+            type="password"
+            size="lg"
+            label="Mật khẩu"
+            name="password"
+            onChange={(e) => setValues({ ...values, password: e.target.value })}
+          />
+        </div>
+        <Button className="mt-6" type="submit">
+          Đăng nhập
+        </Button>
+      </form>
+    </Card>
   );
 }
 

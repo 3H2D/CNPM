744369b5908f4a1d1da8842c20e22d38a3084e47
betty2310 <huynh.dh2310@gmail.com>
2023-06-03 13:28:41 +0700

    feat: init doashboard

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,81 @@
+import {
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+  ListItemSuffix,
+  Chip,
+} from "@material-tailwind/react";
+import {
+  UserIcon,
+  UserCircleIcon,
+  Cog6ToothIcon,
+  InboxIcon,
+  PowerIcon,
+  BellAlertIcon,
+} from "@heroicons/react/24/solid";
+import { TDP_NAME } from "../const";
+
+function Sidebar(props) {
+  const handleHoKhauClick = () => {
+    props.click("hokhau");
+  };
+  return (
+    <Card className="top-4 left-4 h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-150">
+      <Typography variant="h5" className="text-blue-500">
+        {TDP_NAME}
+      </Typography>
+      <List>
+        <ListItem onClick={handleHoKhauClick}>
+          <ListItemPrefix>
+            <UserIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Hộ khẩu
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <BellAlertIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Phản ánh - kiến nghị
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <InboxIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Hòm thư
+          <ListItemSuffix>
+            <Chip
+              value="14"
+              size="sm"
+              variant="ghost"
+              color="blue-gray"
+              className="rounded-full"
+            />
+          </ListItemSuffix>
+        </ListItem>
+        <hr className="my-2 border-blue-gray-50" />
+        <ListItem>
+          <ListItemPrefix>
+            <UserCircleIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Tài khoản
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <Cog6ToothIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Cài đặt
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <PowerIcon className="h-5 w-5" />
+          </ListItemPrefix>
+          Đăng xuất
+        </ListItem>
+      </List>
+    </Card>
+  );
+}
+
+export default Sidebar;
